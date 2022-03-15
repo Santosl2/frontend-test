8404ba82bc6cc172d171cb4993683daabd080cd0
Santosl2c <mfilype2019@gmail.com>
2022-03-15 19:49:52 -0300

    fix: build fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,10 @@
 import "../../styles/globals.scss";
 import type { AppProps } from "next/app";
 import { TokenProvider } from "../context/TokenContext";
-import { Head } from "next/document";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="container">
-      <Head>
-        <title>Klever Test</title>
-      </Head>
-
       <TokenProvider>
         <Component {...pageProps} />
       </TokenProvider>

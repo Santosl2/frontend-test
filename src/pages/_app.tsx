3381b16636bde603cc6e0a18097d6cee639f0323
Santosl2c <mfilype2019@gmail.com>
2022-03-15 14:28:13 -0300

    feat: custom _document to change page title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import "../../styles/globals.scss";
 import type { AppProps } from "next/app";
 import { TokenProvider } from "../context/TokenContext";
+import { Head } from "next/document";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="container">
+      <Head>
+        <title>Klever Test</title>
+      </Head>
+
       <TokenProvider>
         <Component {...pageProps} />
       </TokenProvider>
